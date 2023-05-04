bde248699c890ffb0b85ebf5efdf500d98966425
Ivan Alexandrovich <funtik.gogogo@gmail.com>
2023-05-04 20:29:21 +0300

    vagoni pod kislozovodskom

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
+
+    before_action :set_user!, only: %i[edit update]
+
     def new
         @user = User.new
+    end
+
+    def edit
+
+    end
+
+    def update
+        if @user.update user_params
+            flash[:success] = "Your profile was successfully updated!"
+            redirect_to edit_user_path(@user)
+          else
+            render :edit
+          end       
     end
 
     def create
@@ -15,6 +31,11 @@ class UsersController < ApplicationController
     end
 
     private
+
+    def set_user!
+        @user = User.find params[:id]
+      end
+    
 
     def user_params
         params.require(:user).permit(:email, :name, :password, :password_confirm)
